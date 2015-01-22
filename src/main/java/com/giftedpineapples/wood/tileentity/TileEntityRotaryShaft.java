e1cf8dae317e2e7b21efbea433d0eb138b6b52c8
Tamfoolery <tamfoolery@live.co.uk>
2015-01-22 17:18:25 +0000

    Rotary Shafts now connect to Mechanical Roots

--- src/main/java/com/giftedpineapples/wood/tileentity/TileEntityRotaryShaft.java
+++ src/main/java/com/giftedpineapples/wood/tileentity/TileEntityRotaryShaft.java
@@ -1,5 +1,6 @@
 package com.giftedpineapples.wood.tileentity;
 
+import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class TileEntityRotaryShaft extends TileEntityWC {
@@ -14,22 +15,47 @@ public class TileEntityRotaryShaft extends TileEntityWC {
 	@Override
 	public void updateEntity()
 	{
-		this.updateConnections();
+		this.updateConnectionsToRotaryShafts();
 	}
 
-	public void updateConnections()
+	public void updateConnectionsToRotaryShafts()
 	{
-		if (this.worldObj.getTileEntity(xCoord, yCoord+1, zCoord) instanceof TileEntityRotaryShaft) connectDirection[0] = ForgeDirection.UP;
+		TileEntity up = this.worldObj.getTileEntity(xCoord, yCoord + 1, zCoord);
+		TileEntity down = this.worldObj.getTileEntity(xCoord, yCoord-1, zCoord);
+		TileEntity north = this.worldObj.getTileEntity(xCoord, yCoord, zCoord+1);
+		TileEntity south = this.worldObj.getTileEntity(xCoord, yCoord, zCoord-1);
+		TileEntity east = this.worldObj.getTileEntity(xCoord+1, yCoord, zCoord);
+		TileEntity west = this.worldObj.getTileEntity(xCoord-1, yCoord, zCoord);
+
+		Boolean upConnected = (up instanceof TileEntityRotaryShaft
+				|| up instanceof TileEntityMechanicalRoot);
+
+		Boolean downConnected = (down instanceof TileEntityRotaryShaft
+				|| down instanceof TileEntityMechanicalRoot);
+
+		Boolean northConnected = (north instanceof TileEntityRotaryShaft
+				|| north instanceof TileEntityMechanicalRoot);
+
+		Boolean southConnected = (south instanceof TileEntityRotaryShaft
+				|| south instanceof TileEntityMechanicalRoot);
+
+		Boolean eastConnected = (east instanceof TileEntityRotaryShaft
+				|| east instanceof TileEntityMechanicalRoot);
+
+		Boolean westConnected = (west instanceof TileEntityRotaryShaft
+				|| west instanceof TileEntityMechanicalRoot);
+
+		if (upConnected) connectDirection[0] = ForgeDirection.UP;
 		else connectDirection[0] = null;
-		if (this.worldObj.getTileEntity(xCoord, yCoord-1, zCoord) instanceof TileEntityRotaryShaft) connectDirection[1] = ForgeDirection.DOWN;
+		if (downConnected) connectDirection[1] = ForgeDirection.DOWN;
 		else connectDirection[1] = null;
-		if (this.worldObj.getTileEntity(xCoord, yCoord, zCoord+1) instanceof TileEntityRotaryShaft) connectDirection[2] = ForgeDirection.NORTH;
+		if (northConnected) connectDirection[2] = ForgeDirection.NORTH;
 		else connectDirection[2] = null;
-		if (this.worldObj.getTileEntity(xCoord, yCoord, zCoord-1) instanceof TileEntityRotaryShaft) connectDirection[3] = ForgeDirection.SOUTH;
+		if (southConnected) connectDirection[3] = ForgeDirection.SOUTH;
 		else connectDirection[3] = null;
-		if (this.worldObj.getTileEntity(xCoord+1, yCoord, zCoord) instanceof TileEntityRotaryShaft) connectDirection[4] = ForgeDirection.EAST;
+		if (eastConnected) connectDirection[4] = ForgeDirection.EAST;
 		else connectDirection[4] = null;
-		if (this.worldObj.getTileEntity(xCoord-1, yCoord, zCoord) instanceof TileEntityRotaryShaft) connectDirection[5] = ForgeDirection.WEST;
+		if (westConnected) connectDirection[5] = ForgeDirection.WEST;
 		else connectDirection[5] = null;
 	}
 
